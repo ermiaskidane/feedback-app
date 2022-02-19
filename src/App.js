03c7a83e3c9fd81220670fe4e1b500c62c5ad114
ermias kidane <ermiaskidanegebre@gmail.com>
2022-02-19 15:47:41 +0000

    added router version 6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import FeedbackList from './components/FeedbackList'
 import Header from './components/Header'
 import FeedbackData from './Data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import Post from './components/Post'
+import AboutPage from './pages/AboutPage'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -13,13 +24,51 @@ function App() {
     }
   }
   return (
-    <>
+    // react router dom of version 6
+    <Router>
       <Header />
       <div className='container'>
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+                <AboutIconLink />
+              </>
+            }
+          ></Route>
+          <Route
+            path='/about'
+            element={
+              <>
+                <AboutPage />
+              </>
+            }
+          />
+          <Route path='/post/*' element={<Post />} />
+        </Routes>
       </div>
-    </>
+    </Router>
+
+    // this is react router dom of version 5
+    // {/* <Router>
+    // <Header />
+    // <div className='container'>
+    //   <Route exact path='/'>
+    //     <FeedbackForm handleAdd={addFeedback} />
+    //     <FeedbackStats feedback={feedback} />
+    //     <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+    //   </Route>
+    //   <Route path='/about' component={AboutPage} />
+    // </div>
+    // </Router> */}
   )
 }
 export default App
---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from 'react'
+
+const FeedbackContext = createContext()
+
+const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This item is from context',
+      rating: 10,
+    },
+  ])
+  return (
+    <FeedbackContext.Provider value={{ feedback }}>
+      {children}
+    </FeedbackContext.Provider>
+  )
+}
+
+export default FeedbackProvider
